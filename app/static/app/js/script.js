4d885d53543150e45a4cd0aca9d0231e433486d1
Jude Chuks <judechuks26@gmail.com>
2024-06-05 19:47:28 +0100

    toggle the active class for the hamburger

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -2,6 +2,7 @@ import { headerData } from "../constants/index.js";
 
 // DOM variables
 const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger");
 
 // populating the header with content
 let output = "";
@@ -9,3 +10,5 @@ headerData?.forEach((data) => {
   output += `<li><a href=${data.link}>${data.text}</a></li>`;
 });
 navLinks.innerHTML = output;
+
+hamburger.addEventListener("click", () => hamburger.classList.toggle("active"));
