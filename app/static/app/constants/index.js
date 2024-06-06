b0f1b72f04958f111d1b89b03c97f36346616848
Jude Chuks <judechuks26@gmail.com>
2024-06-06 22:32:51 +0100

    removed the aboutUs nav link

--- app/static/app/constants/index.js
+++ app/static/app/constants/index.js
@@ -1,6 +1,5 @@
 export const headerData = [
   { link: "/", text: "home" },
-  { link: "/about", text: "about" },
   { link: "/services", text: "services" },
   { link: "/products", text: "products" },
   { link: "/contact", text: "contact" },
