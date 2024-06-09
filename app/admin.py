1d1f6393aca2dc3c87040de3b0c12047abba976a
Jude Chuks <judechuks26@gmail.com>
2024-06-09 18:19:20 +0100

    registered the category and product model in the admin dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import Product, Category
 
 # Register your models here.
+# Category model
+@admin.register(Category) # registers the product model
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'category_image',)  # Display the category name
+
+# admin.site.register(Category, CategoryAdmin)
+
+# Product model
+@admin.register(Product) # registers the product model
+class ProductModelAdmin(admin.ModelAdmin):
+  list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
