8342f9e98b115c891c8dc1185fb7306afd093c10
Jude Chuks <judechuks26@gmail.com>
2024-06-09 18:17:18 +0100

    included the url to the upload images in the media folder

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 # urls for app
 from django.urls import path
 from . import views
+from .views import category_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
-]
+    path('', category_list, name='home'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
