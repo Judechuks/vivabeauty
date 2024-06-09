d9b6c052f69a70599510888a5f91985bd7b0c376
Jude Chuks <judechuks26@gmail.com>
2024-06-09 18:23:37 +0100

    defining views for the homepage and the category_list

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+# request for the homepage
 def home(request):
-  return render(request, "app/home.html")
+  categories = Category.objects.all()
+  return render(request, "app/home.html", {'categories': categories})
+
+# request for the category_list
+def category_list(request):
+  categories = Category.objects.all()
+  # get all the categories in the Category Model and pass it to the home.html
+  return render(request, 'app/home.html', {'categories': categories})
