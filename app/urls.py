665915a183c4214184a34f8a08a5c0688cba563a
Jude Chuks <judechuks26@gmail.com>
2024-06-09 19:19:56 +0100

    category url

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,10 @@
 # urls for app
 from django.urls import path
 from . import views
-from .views import category_list
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
-    path('', category_list, name='home'),
+    path('category/', views.CategoryView.as_view(), name="category"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
