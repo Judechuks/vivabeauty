af8afc3d4f206bd5d12303b2b08e7cac9151c3bf
Jude Chuks <judechuks26@gmail.com>
2024-06-10 13:07:43 +0100

    url for category_list, product_list and product_detail views

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
-    path('category/', views.CategoryView.as_view(), name="category"),
+  path('', views.home),
+  path('categories/', views.category_list, name='category_list'),
+  path('products/<int:category_id>/', views.product_list, name='product_list'),
+  path('products/<int:product_id>/', views.product_detail, name='product_detail'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
