e6ed5db2e8c08c4021ec74095c953449198767d3
Jude Chuks <judechuks26@gmail.com>
2024-06-10 20:44:49 +0100

    changed the path url from product to category

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
   path('', views.home),
   path('categories/', views.category_list, name='category_list'),
-  path('products/<int:category_id>/', views.product_list, name='product_list'),
-  path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+  path('category/<int:category_id>/', views.product_list, name='product_list'),
+  path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
