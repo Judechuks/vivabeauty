315306b8d3a5bffa5a46a224d8192fd2f8ec8f23
Jude Chuks <judechuks26@gmail.com>
2024-06-12 16:48:20 +0100

    contact us model

--- app/models.py
+++ app/models.py
@@ -23,3 +23,20 @@ class Product(models.Model):
   # how it's gonna be displayed in the django admin
   def __str__(self):
     return self.title
+
+# Contact Us Model
+class Contact(models.Model):
+  address = models.CharField(max_length=2048)
+  phone_number = models.CharField(max_length=15)
+  email = models.CharField(max_length=255)
+  facebook = models.CharField(max_length=255, blank=True) 
+  whatsapp = models.CharField(max_length=255, blank=True) 
+  twitter = models.CharField(max_length=255, blank=True) 
+  instagram = models.CharField(max_length=255, blank=True) 
+  tiktok = models.CharField(max_length=255, blank=True) 
+  linkedin = models.CharField(max_length=255, blank=True)
+
+  # how it's gonna be displayed in the django admin
+  def __str__(self):
+    # return f"Contact - {self.phone_number}"
+    return f"ContactUs details"
