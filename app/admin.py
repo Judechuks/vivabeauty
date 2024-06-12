46d4212e4af83999efe6e2989d18a2501482f0e0
Jude Chuks <judechuks26@gmail.com>
2024-06-12 16:49:17 +0100

    registering Contact us in admin dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from . models import Product, Category
+from . models import Product, Category, Contact
 
 # Register your models here.
 # Category model
 @admin.register(Category) # registers the product model
 class CategoryAdmin(admin.ModelAdmin):
+# fields to be displayed in the admin dashboard
   list_display = ('name', 'category_image',)  # Display the category name
 
 # admin.site.register(Category, CategoryAdmin)
@@ -12,4 +13,11 @@ class CategoryAdmin(admin.ModelAdmin):
 # Product model
 @admin.register(Product) # registers the product model
 class ProductModelAdmin(admin.ModelAdmin):
+# fields to be displayed in the admin dashboard
   list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+
+# Contact model
+@admin.register(Contact) # registers the product model
+class ContactModelAdmin(admin.ModelAdmin):
+# fields to be displayed in the admin dashboard
+  list_display = ['id', 'phone_number', 'email', 'address', 'facebook']
