164a2e5e92291ea1e3d7d1f1fb856d050f75a926
Jude Chuks <judechuks26@gmail.com>
2024-06-15 23:59:36 +0100

    views for services page and services_details

--- app/views.py
+++ app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.views import View
-from .models import Category, Product, Contact
+from .models import ProductCategory, Product, Contact, ServiceCategory, Service
 
 # Create your views here.
 # request for the homepage
 def home(request):
-  categories = Category.objects.all()
+  categories = ProductCategory.objects.all()
   contact_info = Contact.objects.first()
   # context to pass down the contact and categories information
   context = {'categories': categories, 'contact_info': contact_info}
   return render(request, "app/home.html", context)
 
-# category view
+# product category view
 def category_list(request):
-  categories = Category.objects.all()
+  categories = ProductCategory.objects.all()
   contact_info = Contact.objects.first()
   # context to pass down the contact and categories information
   context = {'categories': categories, 'contact_info': contact_info}
@@ -23,12 +23,12 @@ def category_list(request):
 # product view
 def product_list(request, category_id):
   contact_info = Contact.objects.first()
-  categories = Category.objects.all()
+  categories = ProductCategory.objects.all()
   # getting all the products that matches the category_id
   products = Product.objects.filter(category_id=category_id)
-  selected_category = Category.objects.get(id=category_id)
+  selected_category = ProductCategory.objects.get(id=category_id)
   # context to pass down the contact and categories information
-  context = {'products': products, 'selected_category' : selected_category,'categories': categories, 'contact_info': contact_info}
+  context = {'products': products, 'selected_category' : selected_category, 'categories': categories, 'contact_info': contact_info}
   return render(request, 'app/products.html', context)
 
 # product detail view
@@ -43,7 +43,7 @@ def product_detail(request, product_id):
 def contact(request):
   contact_info = Contact.objects.first()
 
-   # contact us form submission
+  # contact us form submission
   if request.method == 'POST':
     # processing form data
     sender_name = request.POST['Name']
@@ -63,3 +63,29 @@ def contact(request):
   # context to pass down the contact information
   context = {'contact_info': contact_info}
   return render(request, 'app/contact.html', context) 
+
+# service category view
+def service_categories(request):
+  categories = ServiceCategory.objects.all()
+  contact_info = Contact.objects.first()
+  # context to pass down the contact and categories information
+  context = {'categories': categories, 'contact_info': contact_info}
+  return render(request, 'app/services.html', context)
+
+# services view
+def services(request, category_id):
+  contact_info = Contact.objects.first()
+  categories = ServiceCategory.objects.all()
+  # getting all the services that matches the category_id
+  services = Service.objects.filter(category_id=category_id)
+  # context to pass down the contact and categories information
+  context = {'contact_info': contact_info, "services": services}
+  return render(request, 'app/services.html', context)
+  
+# service detail view
+def service_detail(request, service_id):
+  service = Service.objects.get(pk=service_id)
+  contact_info = Contact.objects.first()
+  # context to pass down the contact and categories information
+  context = {'service': service, 'contact_info': contact_info}
+  return render(request, 'app/service_detail.html', context)
