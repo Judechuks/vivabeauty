31ce0e7fb614a34a10d5a0ed4c9ce21a8c05c43f
Jude Chuks <judechuks26@gmail.com>
2024-06-15 23:58:46 +0100

    urls for services page and services_details

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
   path('category/<int:category_id>/', views.product_list, name='product_list'),
   path('product/<int:product_id>/', views.product_detail, name='product_detail'),
   path('contact/', views.contact, name='contact'),
+  path('service_categories/', views.service_categories, name='services_categories'),
+  path('service/<int:category_id>/', views.services, name='services'),
+  path('service_detail/<int:service_id>', views.service_detail, name='service_detail'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
