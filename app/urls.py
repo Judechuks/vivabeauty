ee715671e88731ec474a678c3cee45a76abb0ea6
Jude Chuks <judechuks26@gmail.com>
2024-06-16 21:42:05 +0100

    urls for products, all products, services, all services and service details

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.home),
-  path('categories/', views.category_list, name='category_list'),
-  path('category/<int:category_id>/', views.product_list, name='product_list'),
-  path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+  path('products/', views.all_product_list, name='all_products'),
+  path('services/', views.all_service_list, name='all_services'),
+  path('product_categories/', views.product_categories, name='category_list'),
+  path('category/<int:category_id>/', views.product_list, name='products'),
+  path('products/<int:product_id>/', views.product_detail, name='product_detail'),
   path('contact/', views.contact, name='contact'),
   path('service_categories/', views.service_categories, name='services_categories'),
-  path('service/<int:category_id>/', views.services, name='services'),
+  path('services/<int:category_id>/', views.services, name='services'),
   path('service_detail/<int:service_id>', views.service_detail, name='service_detail'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
