84cf891645a690ad5c164f5a99fde57ae43c20bf
Jude Chuks <judechuks26@gmail.com>
2024-06-18 18:30:45 +0100

    changed the SECRET_KEY and concealed the SECRET_KEY, EMAIL_HOST_USER_ADDRESS and EMAIL_HOST_USER_PASSWORD

--- vivabeautyhome/settings.py
+++ vivabeautyhome/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+import dotenv
 from pathlib import Path
+
+# Loading environment variables from .env file
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ki*ma)$a0dzge3fiz_wfst@psjhz+(_^%cw+-d5t=y8v6y)9g('
+# Using the os.environ dictionary to access environment variables from .env file
+SECRET_KEY = os.environ.get('YOUR_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +131,12 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Settings up email for my contact us form
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ.get('YOUR_EMAIL_HOST_USER_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('YOUR_EMAIL_HOST_USER_PASSWORD')
