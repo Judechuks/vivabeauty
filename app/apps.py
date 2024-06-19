dd190a81dffe4e06b0855cd68f31f3af09874ad9
Jude Chuks <judechuks26@gmail.com>
2024-06-19 17:37:25 +0100

    registering my context processor for passing contact_details to all templates

--- app/apps.py
+++ app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    # context_processors to pass universal context to all template
+    def ready(self):
+        from . import context_processors  # Import your context_processors module
