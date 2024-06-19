d1fe843747c86ed36e78f6c60a6d4663e4be9ed0
Jude Chuks <judechuks26@gmail.com>
2024-06-19 21:57:01 +0100

    login and reset password url

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm, MyPasswordResetForm
 
 urlpatterns = [
   path('', views.home),
@@ -15,6 +17,10 @@ urlpatterns = [
   path('service_categories/', views.service_categories, name='services_categories'),
   path('services/<int:category_id>/', views.services, name='services'),
   path('service_detail/<int:service_id>', views.service_detail, name='service_detail'),
-  # login authentication
+  # Registration and login authentication
   path('registration', views.customerRegistrationView.as_view(), name='register'),
+  # already made login view from django - LoginView
+  path('accounts/login/', auth_view.LoginView.as_view(template_name='app/account_login.html', authentication_form=LoginForm), name='login'), 
+  # already made password reset view from django - PasswordResetView
+  path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'), 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
