8eb35fdf2b70f1a406b6ba51628edfee750f14b4
Jude Chuks <judechuks26@gmail.com>
2024-06-23 22:42:43 +0100

    customer password reset, password reset done, reset_confirm and reset complete url

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, CustomerPasswordResetForm, CustomerPasswordChangeForm
+from .forms import LoginForm, CustomerPasswordResetForm, CustomerPasswordChangeForm, CustomerSetPasswordForm
 
 urlpatterns = [
   path('', views.home),
@@ -21,13 +21,18 @@ urlpatterns = [
   path('registration', views.customerRegistrationView.as_view(), name='register'),
   # already made login view from django - LoginView
   path('accounts/login/', auth_view.LoginView.as_view(template_name='app/account_login.html', authentication_form=LoginForm), name='login'), 
-  # already made password reset view from django - PasswordResetView
-  path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/reset_password.html', form_class=CustomerPasswordResetForm), name='password_reset'), 
-  path('password_change/', auth_view.PasswordChangeView.as_view(template_name='app/change_password.html', form_class=CustomerPasswordChangeForm, success_url='/password_change_done'), name='password_change'), 
-  path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(template_name='app/password_change_done.html'), name='password_change_done'), 
-  path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'), 
   # profile page url
   path('profile/', views.ProfileView.as_view(), name='profile'),
   path('profile_details/', views.profile_details, name='profile_details'),
   path('profile_update/', views.ProfileUpdate.as_view(), name='profile_update'),
+  # already made password change view from django - PasswordChangeView
+  path('password_change/', auth_view.PasswordChangeView.as_view(template_name='app/change_password.html', form_class=CustomerPasswordChangeForm, success_url='/password_change_done'), name='password_change'), 
+  path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(template_name='app/password_change_done.html'), name='password_change_done'), 
+  path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'), 
+  # password reset - already made password change view from django - PasswordResetView
+  path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=CustomerPasswordResetForm), name='password_reset'), 
+  path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'), 
+  path('password_reset_confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=CustomerSetPasswordForm), name='password_reset_confirm'), 
+  path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'), 
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # to include the url to the upload images in the media folder
