e992b586f59f66d91c4f9b731c272212c57d830f
Jude Chuks <judechuks26@gmail.com>
2024-06-26 16:55:42 +0100

    context processor for cart length

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from app.models import Contact, Cart
+
 # To avoid passing the contact details as context to every template, you can use Django’s context processors. Context processors allow you to add data to the context of every template without explicitly passing it in each view function.
-
-from app.models import Contact
-
 def admin_contact_details(request):
   try:
     contact = Contact.objects.get(pk=1)  # Assuming there's only one contact record
@@ -22,3 +22,18 @@ def admin_contact_details(request):
 # In your app’s apps.py, add the context processor to the context_processors list.
 # In your project’s settings.py, add your context processor to the context_processors list.
 # You can then acces the contact details in any template, using {{address}}
+
+# to get the total item a user have in a cart, to avaoid passing it in all the views, I'm using the context_processor
+def cart_length(request):
+  user = request.user
+  if user.is_anonymous:
+    return {'totalCartItem': 0} # Return 0 total cart item if user is not logged in
+  else:
+    try:
+      cart = Cart.objects.filter(user=user)
+      return {
+        'totalCartItem': len(cart)
+      }
+    except Cart.DoesNotExist:
+      return {'totalCartItem': 0}  # Return 0 total cart item if no cart record found
+  # In your project’s settings.py, add your context processor to the context_processors list. register the cart_length
